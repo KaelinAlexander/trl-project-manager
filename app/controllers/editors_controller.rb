d91fc92cf48028b4b3c31d4b7b0864540670fa50
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-14 23:14:19 +0000

    adds initial routes for editors controller

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -7,8 +7,27 @@ class EditorsController < ApplicationController
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :index
+  get '/editors' do
+    erb :'/editors/index'
   end
+
+  get '/editors/new' do
+    erb :'/editors/new'
+  end
+
+  get '/editors/:id' do
+    @editor = Editor.find_by_id(params[:id])
+    erb :'/editors/show'
+  end
+
+  get '/editors/:id/edit' do
+    @editor = Editor.find_by_id(params[:id])
+    erb :'/editors/edit'
+  end
+
+  get '/editors/:id/delete' do
+    @editor = Editor.find_by_id(params[:id])
+    erb :'/editors/delete'
+
 
 end
